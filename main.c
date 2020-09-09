c2e606b714a9a43e3bb741e658392a18e07dcb9b
iuri-pdista <correaiuri86@gmail.com>
2020-09-09 18:57:17 -0300

    Tested constroi function, but failed

--- main.c
+++ main.c
@@ -3,17 +3,24 @@
 #include"functions.c"
 
 int main(){
+	// Ex1 
 	Lista* listPtr = (Lista*) malloc(sizeof(Lista));
 	Lista* listPtr2 = (Lista*) malloc(sizeof(Lista));
 	Lista* listPtr3 = (Lista*) malloc(sizeof(Lista));
 	Lista* listPtr4 = (Lista*) malloc(sizeof(Lista));
 	Lista* listPtr5 = (Lista*) malloc(sizeof(Lista));
 
-	inicializaNoDaLista(listPtr, listPtr2, 10);
-	inicializaNoDaLista(listPtr2, listPtr3, 15);
-	inicializaNoDaLista(listPtr3, listPtr4, 17);
-	inicializaNoDaLista(listPtr4, listPtr5, 19);
+	novoNoDaLista(listPtr, listPtr2, 10);
+	novoNoDaLista(listPtr2, listPtr3, 15);
+	novoNoDaLista(listPtr3, listPtr4, 17);
+	novoNoDaLista(listPtr4, listPtr5, 19);
 	Lista* newList = (Lista*) malloc(sizeof(Lista));
 	newList = separa( listPtr, 15);
-	printf("%d", (*newList).info);
+	printf("%d \n \n", (*newList).info);
+
+	// EX3
+	int elementos[] = {1,2,3,4};
+	Lista* ponteiroLista = constroi( 4, elementos );
+	printf("%d",(*ponteiroLista).info);
+	
 }
