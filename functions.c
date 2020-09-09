df85495f4ff39e916a3856f31f06bfa5a2eb33dc
iuri-pdista <correaiuri86@gmail.com>
2020-09-09 18:52:17 -0300

    Created constroi function

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 typedef struct lista{
 	int info;
-	struct lista* proximo;
+	struct lista* proximo;	
 } Lista;
 
 void novoNoDaLista( Lista* listPtr, Lista* novoNo, int infoDoNo ){
@@ -13,8 +13,17 @@ Lista* separa( Lista* listPtr, int n ){
 	for ( Lista* proxNo = listPtr ; (*proxNo).proximo != NULL ; proxNo = (*proxNo).proximo ){
 		if( (*proxNo).info == n ){
 			splitPoint = (*proxNo).proximo;
+			(*proxNo).proximo = NULL;
 			break;
 		}
 	}
 	return splitPoint;
+}
+
+Lista* constroi( int numDeElem, int* elementos ){
+	Lista* novaLista = (Lista*) malloc( sizeof(Lista) );
+	for ( int i = 0; i < numDeElem; i++ ){
+		(*novaLista).info = elementos[i];
+	}
+	return novaLista;
 }
