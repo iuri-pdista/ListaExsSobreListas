0f63643ad71f75c6408aed1562eea6bd6e854ec8
iuri-pdista <correaiuri86@gmail.com>
2020-09-09 11:32:51 -0300

    Renamed inicializaNoDaLista to novoNoDaLista

--- functions.c
+++ functions.c
@@ -1,20 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-
 typedef struct lista{
 	int info;
 	struct lista* proximo;
 } Lista;
 
-void inicializaNoDaLista( Lista* listPtr, Lista* novoNo, int infoDoNo ){
+void novoNoDaLista( Lista* listPtr, Lista* novoNo, int infoDoNo ){
 	(*listPtr).proximo = novoNo;
 	(*novoNo).info = infoDoNo;
 }
 
 Lista* separa( Lista* listPtr, int n ){
 	Lista* splitPoint = (Lista*) malloc(sizeof(Lista));
-	printf( "%d",(*listPtr).info );
-	for ( Lista* proxNo ; (*proxNo).proximo != NULL ; proxNo = (*proxNo).proximo ){
+	for ( Lista* proxNo = listPtr ; (*proxNo).proximo != NULL ; proxNo = (*proxNo).proximo ){
 		if( (*proxNo).info == n ){
 			splitPoint = (*proxNo).proximo;
 			break;
