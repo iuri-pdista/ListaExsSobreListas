82217e0ea6cb64a66d905955e3292d72e46d196f
iuri-pdista <correaiuri86@gmail.com>
2020-09-09 11:30:22 -0300

    Exercise 1 is correct :white_check_mark:

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 #include"functions.c"
 
 int main(){
-	Lista* listaOriginal, novaLista;
-	novaLista = separa(listaOriginal, 5);
+	Lista* listPtr = (Lista*) malloc(sizeof(Lista));
+	Lista* listPtr2 = (Lista*) malloc(sizeof(Lista));
+	Lista* listPtr3 = (Lista*) malloc(sizeof(Lista));
+	Lista* listPtr4 = (Lista*) malloc(sizeof(Lista));
+	Lista* listPtr5 = (Lista*) malloc(sizeof(Lista));
+
+	inicializaNoDaLista(listPtr, listPtr2, 10);
+	inicializaNoDaLista(listPtr2, listPtr3, 15);
+	inicializaNoDaLista(listPtr3, listPtr4, 17);
+	inicializaNoDaLista(listPtr4, listPtr5, 19);
+	Lista* newList = (Lista*) malloc(sizeof(Lista));
+	newList = separa( listPtr, 15);
+	printf("%d", (*newList).info);
 }
