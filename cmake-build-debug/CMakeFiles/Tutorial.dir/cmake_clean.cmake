f8ea551284a8acf22820a49410b904c5321daf01
guiguat <guaturadzn@gmail.com>
2020-09-10 23:18:28 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/main.c.obj"
+  "Tutorial.exe"
+  "Tutorial.exe.manifest"
+  "Tutorial.lib"
+  "Tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
